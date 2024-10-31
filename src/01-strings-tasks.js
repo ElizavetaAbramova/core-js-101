7f447c5eb27f68fb53f635dbcce137bcf8d365c5
Abramova Elizaveta <a.d.elizaveta@gmail.com>
2024-10-31 16:26:37 +0400

    feat: add changes

--- src/01-strings-tasks.js
+++ src/01-strings-tasks.js
@@ -108,23 +108,23 @@ function getRectangleString(width, height) {
  * Encode specified string with ROT13 cipher
  */
 function encodeToRot13(str) {
-  let decodedWord = '';
+  let encodedWord = '';
   for (let i = 0; i < str.length; i += 1) {
     const code = str.charCodeAt(i);
     // decodedWord = `${decodedWord}${String.fromCharCode(str.charCodeAt(i) + 13)}`;
     if (code > ('A'.charCodeAt(0) + 'Z'.charCodeAt(0)) / 2 && code <= 'Z'.charCodeAt(0)) {
-      decodedWord = `${decodedWord}${String.fromCharCode(code - 13)}`;
+      encodedWord = `${encodedWord}${String.fromCharCode(code - 13)}`;
     } else if (code < ('A'.charCodeAt(0) + 'Z'.charCodeAt(0)) / 2 && code >= 'A'.charCodeAt(0)) {
-      decodedWord = `${decodedWord}${String.fromCharCode(code + 13)}`;
+      encodedWord = `${encodedWord}${String.fromCharCode(code + 13)}`;
     } else if (code > ('a'.charCodeAt(0) + 'z'.charCodeAt(0)) / 2 && code <= 'z'.charCodeAt(0)) {
-      decodedWord = `${decodedWord}${String.fromCharCode(code - 13)}`;
+      encodedWord = `${encodedWord}${String.fromCharCode(code - 13)}`;
     } else if (code < ('a'.charCodeAt(0) + 'z'.charCodeAt(0)) / 2 && code >= 'a'.charCodeAt(0)) {
-      decodedWord = `${decodedWord}${String.fromCharCode(code + 13)}`;
+      encodedWord = `${encodedWord}${String.fromCharCode(code + 13)}`;
     } else {
-      decodedWord = `${decodedWord}${str[i]}`;
+      encodedWord = `${encodedWord}${str[i]}`;
     }
   }
-  return decodedWord;
+  return encodedWord;
 }
 
 /**
